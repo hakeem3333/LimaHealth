2872430d80d8732d9ddf3169a629155bc1cff51b
hakeem3333 <Hakeemsalaudeen460@gmail.com>
2025-11-28 07:26:26 +0100

    refactor(schema): update Prisma schema with full RBAC models and cascade rules

--- prisma/migrations/20251127155822_make_school_id_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."User" DROP CONSTRAINT "User_schoolId_fkey";
-
--- AlterTable
-ALTER TABLE "public"."User" ALTER COLUMN "schoolId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "public"."User" ADD CONSTRAINT "User_schoolId_fkey" FOREIGN KEY ("schoolId") REFERENCES "public"."School"("id") ON DELETE SET NULL ON UPDATE CASCADE;
